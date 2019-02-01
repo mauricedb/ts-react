26fd3959e07dab1d54373f1ddce773d197a7861a
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-02-01 19:34:53 +0100

    Simple counter

---
+++ src/Counter/Counter.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+type CounterState = number;
+
+type CounterProps = {
+  amount: number;
+};
+
+const Counter = ({ amount }: CounterProps) => {
+  const [count, setCount] = useState<CounterState>(0);
+
+  return (
+    <div className="text-center">
+      <div>Count: {count}</div>
+      <div>
+        <button onClick={() => setCount(count + amount)}>Increment</button>
+      </div>
+    </div>
+  );
+};
+
+Counter.defaultProps = {
+  amount: 1
+};
+
+export default Counter;
