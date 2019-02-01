18662277a28dde5caa27277a090e13b8c37d2ab0
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-02-01 19:50:42 +0100

    Unit test the counter

---
+++ src/Counter/Counter.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import 'jest-dom/extend-expect';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+
+import Counter from './Counter';
+
+afterEach(cleanup);
+
+test('can render initial count', () => {
+  const { getByText } = render(<Counter />);
+
+  expect(getByText('Count: 0')).toBeInTheDocument();
+});
+
+test('can increment the default amount', () => {
+  const { getByText } = render(<Counter />);
+
+  fireEvent.click(getByText('Increment'));
+
+  expect(getByText('Count: 1')).toBeInTheDocument();
+});
+
+test('can increment twice', () => {
+  const { getByText } = render(<Counter />);
+
+  fireEvent.click(getByText('Increment'));
+  fireEvent.click(getByText('Increment'));
+
+  expect(getByText('Count: 2')).toBeInTheDocument();
+});
+
+test('can increment the specified amountof 5', () => {
+  const { getByText } = render(<Counter amount={5} />);
+
+  fireEvent.click(getByText('Increment'));
+
+  expect(getByText('Count: 5')).toBeInTheDocument();
+});
