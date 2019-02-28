4b53666e71812f16f3b42d1ff80f1d63ca4b2f5b
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-02-28 21:07:21 +0100

    Navbar and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
+
 import Counter from './Counter/Counter';
 import Person from './Person/Person';
 import Movies from './movies/Movies';
@@ -8,17 +11,44 @@ import ShapeEditor from './shape-editor/ShapeEditor';
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <header className="text-center">
-          <h1>React & TypeScript</h1>
-        </header>
-        {/* <Counter /> */}
-        {/* <Counter amount={2} /> */}
-        {/* <Person /> */}
-        {/* <Movies /> */}
-        <MovieEditor />
-        {/* <ShapeEditor /> */}
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand as={Link} to="/">
+              React & TypeScript
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Nav className="mr-auto">
+              <Nav.Link as={Link} to="/counter">
+                Counter
+              </Nav.Link>
+              <Nav.Link as={Link} to="/person">
+                Person
+              </Nav.Link>
+              <Nav.Link as={Link} to="/movies">
+                Movies
+              </Nav.Link>
+              <Nav.Link as={Link} to="/movie-editor">
+                Movie Editor
+              </Nav.Link>
+              <Nav.Link as={Link} to="/shape-editor">
+                Shape Editor
+              </Nav.Link>
+            </Nav>
+            <Nav />
+          </Navbar>
+          <header className="text-center">
+            <h1>React & TypeScript</h1>
+          </header>
+          <Switch>
+            <Route path="/counter" component={Counter} />
+            <Route path="/person" component={Person} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/movie-editor" component={MovieEditor} />
+            <Route path="/shape-editor" component={ShapeEditor} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
