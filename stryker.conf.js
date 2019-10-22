08e575bcef0e7e3d99d7324d7665eb83b3b424b6
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-10-22 11:28:38 +0200

    stryker init

---
+++ stryker.conf.js
@@ -0,0 +1,14 @@
+// This config was generated using a preset.
+// Please see the handbook for more information: https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/guides/react.md#react
+module.exports = function(config) {
+  config.set({
+    mutate: ['src/**/*.ts?(x)', '!src/**/*@(.test|.spec|Spec).ts?(x)'],
+    mutator: 'typescript',
+    testRunner: 'jest',
+    reporters: ['progress', 'clear-text', 'html'],
+    coverageAnalysis: 'off',
+    jest: {
+      projectType: 'react'
+    }
+  });
+};
