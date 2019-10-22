4de16784454edc8a0a33bacaafdefb687dcfd0d4
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-10-22 12:47:28 +0200

    Skip serviceWorker.ts

--- stryker.conf.js
+++ stryker.conf.js
@@ -2,7 +2,11 @@
 // Please see the handbook for more information: https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/guides/react.md#react
 module.exports = function(config) {
   config.set({
-    mutate: ['src/**/*.ts?(x)', '!src/**/*@(.test|.spec|Spec).ts?(x)'],
+    mutate: [
+      'src/**/*.ts?(x)',
+      '!src/**/*@(.test|.spec|Spec).ts?(x)',
+      '!src/serviceWorker.ts'
+    ],
     mutator: 'typescript',
     testRunner: 'jest',
     reporters: ['progress', 'clear-text', 'html'],
