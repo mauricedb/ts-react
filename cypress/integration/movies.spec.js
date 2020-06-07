c0f517507012f9e34b41b0a87458e5ae741fcc32
Maurice de Beijer <maurice.de.beijer@gmail.com>
2020-06-07 17:59:26 +0200

    Split specs

---
+++ cypress/integration/movies.spec.js
@@ -0,0 +1,14 @@
+context('Movies', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+    // cy.contains('Counter').click();
+  });
+
+  it('opens the page', () => {
+    cy.get('[href="/movies"]').click();
+
+    cy.contains("Schindler's List").click();
+
+    cy.get('ul > :nth-child(1)').should('have.text', 'Dra ma');
+  });
+});
