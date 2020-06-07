d8a6b32a698a7318ffd912d3703da746085f0f8d
Maurice de Beijer <maurice.de.beijer@gmail.com>
2020-06-07 18:04:21 +0200

    Fix intentional error

--- cypress/integration/movies.spec.js
+++ cypress/integration/movies.spec.js
@@ -9,6 +9,6 @@ context('Movies', () => {
 
     cy.contains("Schindler's List").click();
 
-    cy.get('ul > :nth-child(1)').should('have.text', 'Dra ma');
+    cy.get('ul > :nth-child(1)').should('have.text', 'Drama');
   });
 });
